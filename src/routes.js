3a9897878bdc6281eea8247a27848b96ca3e281d
christopher.miranda <christopher.miranda@totvs.com.br>
2021-04-07 22:10:51 -0300

    comit referente a aula numero 1

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,25 @@
 const express = require("express")
 const routes = express.Router()
-const basePath = __dirname + "/views"
 
-routes.get('/', (request, response) => response.sendFile(basePath + "/index.html"))
-routes.get('/job', (request, response) => response.sendFile(basePath + "/job.html"))
-routes.get('/job/edit', (request, response) => response.sendFile(basePath + "/job-edit.html"))
-routes.get('/profile', (request, response) => response.sendFile(basePath + "/profile.html"))
+const views = __dirname + "/views/"
+
+const profile = {
+    name: "ChrisMiiranda",
+    avatar: "https://github.com/chrisMiiranda.png",
+    "monthly-budget": 3000,
+    "hours-per-day": 5,
+    "days-per-week": 5,
+    "vocation-per-year": 4
+}
+
+routes.get('/', (req, res) => res.render(views + "index"))
+
+routes.get('/job', (req, res) => res.render(views + "job"))
+routes.post('/job', (req, res) => res.render(views + "job"))
+
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
 
 
 module.exports = routes;
